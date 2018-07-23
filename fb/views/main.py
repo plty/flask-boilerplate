06dacc0e82b70b2bbaccd322ec590cbf7743152a
plty <platyunyu@gmail.com>
2018-07-23 22:03:23 +0800

    draft pulang kantor

--- fb/views/main.py
+++ fb/views/main.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from flask_classy import FlaskView
 
 
@@ -8,4 +9,4 @@ class MainView(FlaskView):
         return "Hello, this is the MainView"
 
     def otto(self):
-        return "hai kamu"
+        return render_template('templates/otto.html')
